598710f2e0c052bc2686c4fa8c8a0efd208c44a4
Eswar-04 <muthueswaru.ug22.cb@francisxavier.ac.in>
2025-05-10 00:05:53 +0530

    Sumofevennumbers.c

--- sumofevennumbers.c
+++ sumofevennumbers.c
@@ -2,12 +2,13 @@
 int main(){
     int i;
     int sum = 0;
-    for(i=0;i<sum;i++)
+    for(i=0;i<=100;i++)
     {
         if(i%2==0){
             sum=sum+i;
         }
-        printf("%d",sum);
+        
     }
+    printf("The Sum of Evem number between 1 to 100 is :%d",sum);
       return 0;
 }
